b7eeafdcaf6b936ee3fb880a2077c8f4af03d072
semiexp <linthya.v@gmail.com>
2018-05-11 18:54:49 +0900

    Add solved mark

--- src/components/Numberlink.js
+++ src/components/Numberlink.js
@@ -13,14 +13,22 @@ export default class Numberlink extends React.Component {
     constructor(props) {
         super(props);
         
-        this.state = { field: new NumberlinkField(props.problem) };
+        this.state = {
+            field: new NumberlinkField(props.problem),
+            isSolved: false
+        };
 
         this.onChange = this.onChange.bind(this);
     }
 
     onChange(x, y, s) {
+        let { field, isSolved } = this.state;
+        if (isSolved) return;
+
+        let nextField = field.setSegment(x, y, s);
         this.setState( {
-            field: this.state.field.setSegment(x, y, s)
+            field: nextField,
+            isSolved: nextField.checkAnswer()
         });
     }
 
@@ -31,14 +39,15 @@ export default class Numberlink extends React.Component {
     }
 
     render() {
-        const { field } = this.state;
+        const { field, isSolved } = this.state;
         const allClues = field.getAllClues();
 
-        const margin = 2;
+        const margin = 6;
         const gridLineWidth = 1;
         const cellSize = 40;
-        
-        if (field.checkAnswer()) console.log('ok!');
+
+        const svgHeight = margin * 2 + gridLineWidth * (field.height + 1) + cellSize * field.height;
+        const svgWidth = margin * 2 + gridLineWidth * (field.width + 1) + cellSize * field.width;
 
         return (
             <LineGridHandlingSVGContainer
@@ -47,7 +56,16 @@ export default class Numberlink extends React.Component {
                 cellSize={cellSize}
                 lineGrid={field}
                 onChange={this.onChange}>
-                <SVGLineGrid   
+                { isSolved && <rect
+                    x={3}
+                    y={3}
+                    height={svgHeight - 3 * 2}
+                    width={svgWidth - 3 * 2}
+                    strokeWidth={3}
+                    stroke='#f00'
+                    fill='none'/>
+                }
+                <SVGLineGrid
                     offsetX={margin}
                     offsetY={margin}
                     gridLineWidth={gridLineWidth}
